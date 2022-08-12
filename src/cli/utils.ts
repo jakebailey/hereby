dc69d1f4ffd45bfee150508cdc07f14b995a9d98
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-08-12 12:03:47 -0700

    Fix exit

--- src/cli/utils.ts
+++ src/cli/utils.ts
@@ -5,10 +5,8 @@ import pc from "picocolors";
 import type { Task } from "../index.js";
 
 export function exitWithError(message: string): never {
-    // TODO: should this be a throw of a custom Error type,
-    // such that we can rely on this in the non-CLI part of the code?
     console.error(`${pc.red("Error")}: ${message}`);
-    process.exit(0);
+    process.exit(1);
 }
 
 export function taskSorter(a: Task, b: Task): number {
