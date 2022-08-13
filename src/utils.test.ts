2d5294a33ac9d716e87434d6ef47b108bd8c5a96
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-08-12 20:34:16 -0700

    Add test for forEachTask

---
+++ src/utils.test.ts
@@ -0,0 +1,49 @@
+import { Task, task } from "./index.js";
+import { forEachTask } from "./utils.js";
+
+test("visits each once", () => {
+    const a = task({
+        name: "a",
+    });
+
+    const b = task({
+        name: "b",
+        dependencies: [a],
+    });
+
+    const c = task({
+        name: "c",
+        dependencies: [b],
+    });
+
+    const d = task({
+        name: "d",
+        dependencies: [c],
+    });
+
+    const e = task({
+        name: "e",
+        dependencies: [a, b],
+    });
+
+    const f = task({
+        name: "f",
+        dependencies: [e, c],
+    });
+
+    const counts = new Map<Task, number>();
+
+    forEachTask([d, f], (task) => {
+        const x = counts.get(task) ?? 0;
+        counts.set(task, x + 1);
+    });
+
+    const allTasks = [a, b, c, d, e, f];
+
+    for (const t of allTasks) {
+        expect(counts.get(t)).toEqual(1);
+        counts.delete(t);
+    }
+
+    expect(counts.size).toEqual(0);
+});
