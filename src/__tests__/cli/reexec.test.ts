ebdc84e07c91a5183e88a55b4064d50e181a9bed
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-08-14 18:03:50 -0700

    Better test reexec

--- src/__tests__/cli/reexec.test.ts
+++ src/__tests__/cli/reexec.test.ts
@@ -1,13 +1,13 @@
 import test from "ava";
 import esmock from "esmock";
-import { It, Mock, Times } from "moq.ts";
+import { GetPropertyExpression, It, Mock, Times } from "moq.ts";
 import { fileURLToPath } from "url";
 
 import type * as reexec from "../../cli/reexec.js";
-import type { System } from "../../cli/utils.js";
+import type { Process, System } from "../../cli/utils.js";
 
-const cliIndexPath = new URL("../../cli/index.js", import.meta.url).toString();
-const wrongCliIndexPath = new URL("../../other/cli/index.js", import.meta.url).toString();
+const cliIndexURL = new URL("../../cli/index.js", import.meta.url).toString();
+const wrongCliIndexURL = new URL("../../other/cli/index.js", import.meta.url).toString();
 const herebyfilePath = fileURLToPath(new URL("./fixtures/normal.mjs", import.meta.url));
 
 test("no re-exec", async (t) => {
@@ -32,9 +32,9 @@ test("no re-exec", async (t) => {
                 callCount++;
                 switch (callCount) {
                     case 1:
-                        return cliIndexPath;
+                        return cliIndexURL;
                     case 2:
-                        return cliIndexPath;
+                        return cliIndexURL;
                     default:
                         throw new Error("too many calls");
                 }
@@ -49,7 +49,23 @@ test("no re-exec", async (t) => {
 test("re-exec", async (t) => {
     let callCount = 0;
 
-    const systemMock = new Mock<System>().setup((instance) => instance.error(It.IsAny())).returns();
+    const execPath = "node";
+    const execArgv = ["--loader=my-cool-loader"];
+    const argv = [execPath, fileURLToPath(cliIndexURL), "build", "test", "--lint", "--lkg"];
+
+    const processMock = new Mock<Process>()
+        .setup((instance) => instance.execPath)
+        .returns(execPath)
+        .setup((instance) => instance.execArgv)
+        .returns(execArgv)
+        .setup((instance) => instance.argv)
+        .returns(argv);
+
+    const systemMock = new Mock<System>()
+        .setup((instance) => instance.error(It.IsAny()))
+        .returns()
+        .setup((instance) => instance.process)
+        .returns(processMock.object());
 
     // This is a workaround for a bug in esmock; esmock appears to follow
     // source maps, so I pass "../../cli/reexec.js" directly, it uses src/...
@@ -59,16 +75,19 @@ test("re-exec", async (t) => {
     const modulePath = new URL("../../cli/reexec.js", import.meta.url);
     const reexecModule: typeof reexec = await esmock(fileURLToPath(modulePath), {
         "foreground-child": {
-            default: () => {},
+            default: (program: string, args: string[]) => {
+                t.is(program, execPath);
+                t.deepEqual(args, [...execArgv, fileURLToPath(wrongCliIndexURL), ...argv.slice(2)]);
+            },
         },
         "import-meta-resolve": {
             resolve: async () => {
                 callCount++;
                 switch (callCount) {
                     case 1:
-                        return cliIndexPath;
+                        return cliIndexURL;
                     case 2:
-                        return wrongCliIndexPath;
+                        return wrongCliIndexURL;
                     default:
                         throw new Error("too many calls");
                 }
