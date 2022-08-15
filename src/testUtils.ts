c1857b864c6d73dcbf84dc2fb118538a183b7cc3
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-08-15 13:29:05 -0700

    Fix formatting

--- src/testUtils.ts
+++ src/testUtils.ts
@@ -1,10 +1,10 @@
 import { fileURLToPath } from "url";
 
-    // This is a workaround for a bug in esmock; esmock appears to follow
-    // source maps, so I pass "../../cli/reexec.js" directly, it uses src/...
-    // rather than dist/...
-    //
-    // TODO: Remove once https://github.com/iambumblehead/esmock/issues/113 is fixed.
+// This is a workaround for a bug in esmock; esmock appears to follow
+// source maps, so I pass "../../cli/reexec.js" directly, it uses src/...
+// rather than dist/...
+//
+// TODO: Remove once https://github.com/iambumblehead/esmock/issues/113 is fixed.
 export function fixESMockPath(path: string, importMetaUrl: string): string {
     const url = new URL(path, importMetaUrl);
     return fileURLToPath(url);
