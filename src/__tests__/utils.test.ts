e382a80347e718a0738738a1ca9f566da259d5e4
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-08-14 21:49:06 -0700

    Test utils

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,9 +1,12 @@
 import test from "ava";
+import { write } from "fs";
+import { It, Mock, Times } from "moq.ts";
 
+import { createSystem, ExitCodeError, Process, UserError } from "../cli/utils.js";
 import { Task, task } from "../index.js";
 import { forEachTask } from "../utils.js";
 
-test("visits each once", (t) => {
+test("forEachTask visits each once", (t) => {
     const a = task({
         name: "a",
         run: async () => {},
@@ -49,4 +52,39 @@ test("visits each once", (t) => {
     }
 
     t.is(counts.size, 0);
+});
+
+test("UserError", (t) => {
+    const e = new UserError("message");
+    t.is(e.message, "message");
+});
+
+test("ExitCodeError", (t) => {
+    const e = new ExitCodeError(1);
+    t.is(e.exitCode, 1);
+});
+
+test("createSystem logging", (t) => {
+    const stdoutMock = new Mock<Process["stdout"]>()
+        .setup((instance) => instance.write(It.IsAny() as string))
+        .returns(true);
+    const stderrMock = new Mock<Process["stderr"]>()
+        .setup((instance) => instance.write(It.IsAny() as string))
+        .returns(true);
+
+    const processMock = new Mock<Process>()
+        .setup((instance) => instance.stdout)
+        .returns(stdoutMock.object())
+        .setup((instance) => instance.stderr)
+        .returns(stderrMock.object());
+
+    const system = createSystem(processMock.object());
+
+    system.log("log");
+    system.error("error");
+
+    stdoutMock.verify((instance) => instance.write(It.Is((v) => v === "log\n")), Times.Once());
+    stderrMock.verify((instance) => instance.write(It.Is((v) => v === "error\n")), Times.Once());
+
+    t.pass();
 });
--- src/cli/utils.ts
+++ src/cli/utils.ts
@@ -29,7 +29,11 @@ export function simplifyPath(p: string) {
  * UserError is a special error that, when caught in the CLI will be printed
  * as a message only, without stacktrace. Use this instead of process.exit.
  */
-export class UserError extends Error {}
+export class UserError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
 
 /**
  * When thrown, ExitCodeError causes the process to exit with a specific error code,
