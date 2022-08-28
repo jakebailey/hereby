0cacf8f682ff01b24d8bb672b25774da391592e1
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-08-28 13:25:14 -0700

    Update deps

--- .ncurc.js
+++ .ncurc.js
@@ -1,6 +1,7 @@
 module.exports = {
     target: (dependencyName, [{ semver, version, operator, major, minor, patch, release, build }]) => {
         if (dependencyName === "@types/node") return "minor";
+        if (dependencyName === "typescript") return "patch";
         if (major === "0") return "minor";
         return "latest";
     },
