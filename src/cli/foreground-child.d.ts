14805c6c46b1d39839b70f6f55267e5cedba122e
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-11-04 09:13:18 -0700

    Switch to @types/foreground-child, now that it's on DT

--- src/cli/foreground-child.d.ts
+++
@@ -1,9 +0,0 @@
-// TODO: submit this to DT
-
-declare module "foreground-child" {
-    function foregroundChild(program: string | string[], cb?: CloseHandler);
-    function foregroundChild(program: string, args: string[], cb?: CloseHandler);
-    function foregroundChild(program: string, ...args: string[], cb?: CloseHandler);
-
-    export = foregroundChild;
-}
