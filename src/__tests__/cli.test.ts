c204af4629bc830ff2e426ce8856a5a11d7d5252
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-11-05 12:32:36 -0700

    Properly test reexec

--- src/__tests__/cli.test.ts
+++ src/__tests__/cli.test.ts
@@ -1,5 +1,8 @@
 import test from "ava";
 import { execaNode } from "execa";
+import fs from "fs";
+import path from "path";
+import tmp from "tmp";
 import { fileURLToPath } from "url";
 
 const cliPath = fileURLToPath(new URL("../cli.js", import.meta.url));
@@ -14,4 +17,39 @@ test("run cli --help", async (t) => {
 test("run cli --version", async (t) => {
     await execaNode(cliPath, ["--version"]);
     t.pass();
+});
+
+test("run cli reexec", async (t) => {
+    const tmpdir = tmp.dirSync({ unsafeCleanup: true });
+    t.teardown(tmpdir.removeCallback);
+    const dir = tmpdir.name;
+
+    const fakeHereby = path.join(dir, "node_modules", "hereby");
+    await fs.promises.mkdir(fakeHereby, { recursive: true });
+
+    await fs.promises.writeFile(
+        path.join(fakeHereby, "package.json"),
+        JSON.stringify({
+            name: "hereby",
+            type: "module",
+            exports: {
+                "./cli": "./fake.js",
+            },
+        }),
+    );
+
+    await fs.promises.writeFile(path.join(fakeHereby, "fake.js"), "console.log('It works!')");
+
+    await fs.promises.writeFile(
+        path.join(dir, "package.json"),
+        JSON.stringify({
+            name: "test",
+            type: "module",
+        }),
+    );
+
+    await fs.promises.writeFile(path.join(dir, "Herebyfile.mjs"), "export {};");
+
+    const { stdout } = await execaNode(cliPath, { cwd: dir });
+    t.is(stdout, "It works!");
 });
