7e5244b83139b39e90df17db4c01bde8a688c6f5
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-11-14 18:39:16 -0800

    Try and support Node 12 for just a little longer

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,7 @@ module.exports = {
     target: (dependencyName, [{ semver, version, operator, major, minor, patch, release, build }]) => {
         if (dependencyName === "@types/node") return "minor";
         if (dependencyName === "typescript") return "patch";
+        if (dependencyName === "ava") return "patch"; // Trying to support Node 12.
         if (major === "0") return "minor";
         return "latest";
     },
