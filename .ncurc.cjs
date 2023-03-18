17b562791f6e0c3550c954d3393ad0cc075b522f
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-03-17 23:19:48 -0700

    Update deps

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -2,6 +2,7 @@ module.exports = {
     target: (dependencyName, [{ semver, version, operator, major, minor, patch, release, build }]) => {
         if (dependencyName === "@types/node") return "minor";
         if (dependencyName === "ava") return "patch"; // Trying to support Node 12.
+        if (dependencyName === "@ava/typescript") return "minor"; // Trying to support Node 12.
         if (dependencyName === "moq.ts") return "minor"; // v10 is typed wrong for node ESM
         if (dependencyName === "execa") return "patch"; // Trying to support Node 12.
         if (dependencyName === "command-line-usage") return "minor"; // Package got big.
