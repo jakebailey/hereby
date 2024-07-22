9a5ff62459635cddf00848af7b55ed30b46b5f21
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-07-21 20:32:36 -0700

    Add reexec benchmarks, but this means the other benchmarks are subtly wrong due to module caching

--- src/__tests__/benchmarks/index.bench.ts
+++ src/__tests__/benchmarks/index.bench.ts
@@ -1,14 +1,20 @@
+import fs from "node:fs";
 import path from "node:path";
 import url from "node:url";
 
 import { withCodSpeed } from "@codspeed/tinybench-plugin";
+import { execa } from "execa";
 import { Bench } from "tinybench";
+import tmp from "tmp";
 
 import { main } from "../../cli/index.js";
 import { type D, real } from "../../cli/utils.js";
 
 const __filename = url.fileURLToPath(new URL(import.meta.url));
 const __dirname = path.dirname(__filename);
+
+const packageRoot = path.join(__dirname, "..", "..", "..");
+const cli = path.join(packageRoot, "dist", "cli.js");
 
 function noop() {}
 
@@ -23,7 +29,7 @@ async function depsForBenchmark(argv: readonly string[], cwd: string): Promise<D
         chdir: (dir: string) => {
             cwd = dir;
         },
-        argv,
+        argv: [process.execPath, cli, ...argv],
         setExitCode: noop,
     };
 }
@@ -34,7 +40,7 @@ type BenchmarkOptions = Parameters<Bench["add"]>[2] & {
     cwd?: string;
 };
 
-function registerBenchmark(name: string, argv: readonly string[], opts: BenchmarkOptions = {}) {
+function registerDirectBenchmark(name: string, argv: readonly string[], opts: BenchmarkOptions = {}) {
     let deps: D;
     const cwd = opts.cwd ?? process.cwd();
     bench.add(name, () => main(deps), {
@@ -50,28 +56,62 @@ function registerBenchmark(name: string, argv: readonly string[], opts: Benchmar
 
 const fixturesDir = path.join(__dirname, "..", "cli", "__fixtures__");
 
-registerBenchmark(
+registerDirectBenchmark(
     "main print version",
-    ["node", "cli.js", "--version"],
+    ["--version"],
 );
 
-registerBenchmark(
+registerDirectBenchmark(
     "main print tasks",
-    ["node", "cli.js", "--tasks"],
+    ["--tasks"],
     { cwd: fixturesDir },
 );
 
-registerBenchmark(
+registerDirectBenchmark(
     "main print tasks simple",
-    ["node", "cli.js", "--tasks-simple"],
+    ["--tasks-simple"],
     { cwd: fixturesDir },
 );
 
-registerBenchmark(
+registerDirectBenchmark(
     "main run wrong name",
-    ["node", "cli.js", "buildCompile"],
+    ["buildCompile"],
     { cwd: fixturesDir },
 );
+
+async function createTempPackage() {
+    const tmpDir = tmp.dirSync({ unsafeCleanup: true });
+    const packageJson = {
+        name: "test-package",
+        version: "1.0.0",
+        devDependencies: {
+            hereby: `file:${packageRoot}`,
+        },
+    };
+    await fs.promises.writeFile(path.join(tmpDir.name, "package.json"), JSON.stringify(packageJson));
+    await fs.promises.cp(path.join(fixturesDir, "Herebyfile.mjs"), path.join(tmpDir.name, "Herebyfile.mjs"));
+
+    await execa("npm", ["install"], { cwd: tmpDir.name });
+    return tmpDir;
+}
+
+function registerReexecBenchmark(name: string, argv: readonly string[]) {
+    let cwd: string;
+
+    bench.add(name, () => execa(process.execPath, [cli, ...argv], { cwd, stdio: "ignore" }), {
+        beforeAll: async () => {
+            const tmpDir = await createTempPackage();
+            cwd = tmpDir.name;
+        },
+        afterAll: async () => {
+            await fs.promises.rm(cwd, { recursive: true, force: true });
+        },
+    });
+}
+
+registerReexecBenchmark("main reexec print tasks", ["--tasks"]);
+
+registerReexecBenchmark("main reexec print tasks simple", ["--tasks-simple"]);
 
 await bench.run();
 console.table(bench.table());
