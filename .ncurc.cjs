e5747a37650ceaed36d9d33a2751f2f8e4bebbd8
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-03-05 21:39:39 -0800

    Clean up old dep references

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -4,7 +4,6 @@ module.exports = {
         if (dependencyName === "ava") return "patch"; // Trying to support Node 12.
         if (dependencyName === "@ava/typescript") return "minor"; // Trying to support Node 12.
         if (dependencyName === "execa") return "patch"; // Trying to support Node 12.
-        if (dependencyName === "pretty-ms") return "minor"; // For old versions of node.
         if (dependencyName === "rimraf") return "minor";
         if (dependencyName === "npm") return "minor";
         if (major === "0") return "minor";
